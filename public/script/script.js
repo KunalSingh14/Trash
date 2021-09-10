f57d6686a994ae5781838327b528353a4a626451
Ankit Choudhary <choudharyankit135aj@gmail.com>
2021-09-10 21:27:31 +0530

    changes

--- public/script/script.js
+++ public/script/script.js
@@ -1,19 +1,20 @@
 const uids = [783104, 289394, 579320, 109573];
 const otps = [123, 345, 234, 765, 456];
 let uid;
+document.getElementById("UID").autocomplete = "on";
 document.getElementById("submit1").addEventListener("click", async () => {
     let myForm = document.getElementById("signUp");
     uid = myForm.elements["UID"].value;
     if (!uids.includes(parseInt(uid))) {
-        alert("Invalid UID");
+        document.getElementById("temp1").innerHTML = `Please enter valid UID`;
         return;
     }
     myForm.innerHTML = `<label for="UID" class="uid_name">Enter OTP</label>
                     <br>
-                    <input type="text" name="UID" id="UID" class="uid_no" minlength="12" maxlength="12" pattern=[0-9]
-                        placeholder="  XXXX-XXXX-XXXX">
+                    <input type="password" name="UID" id="UID" class="uid_no" minlength="12" maxlength="12" pattern=[0-9]
+                        placeholder="  - - - -">
                     <br>
-                    <h4>OTP has been sent to registerd<br> mobile number</h4>
+                    <h4 id="temp2">OTP has been sent to registerd<br> mobile number</h4>
                     <br>
                     <input type="button" value="Resend OTP" id="submit2" class="btn_resend" />
                     <input type="button" value="Verify" id="verify" class="btn_verify" />`;
@@ -25,8 +26,10 @@ document.getElementById("submit1").addEventListener("click", async () => {
         uids.forEach((item, index) => {
             if (item === parseInt(uid)) temp = index;
         });
-        console.log(temp);
         if (otps[temp] === parseInt(otp)) alert("Login Successful!!!");
-        else alert("Invalid OTP!!!");
+        else {
+            document.getElementById("temp2").innerHTML = `Invalid OTP`;
+            return;
+        }
     });
 });
