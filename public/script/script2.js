e00714da4ddda3d17fa2f036bd17d501e78045e7
Ankit Choudhary <choudharyankit135aj@gmail.com>
2021-09-12 14:18:56 +0530

    dashboard script

--- public/script/script2.js
+++ public/script/script2.js
@@ -8,16 +8,41 @@ const getConfig = async () => {
     const data = await response.json();
     config = data;
 };
-const driver = async () => {
-    await getConfig();
+
+const getUserData = async () => {
     const response = await fetch(config.API_BASE + "getUser?uid=" + uid);
     const data = await response.json();
     if (data.success === false) {
-        driver();
+        getUserData();
         return;
     }
     user = data.result;
     document.getElementById("id101").innerText = "Hi, " + user.fname;
+};
+
+const getCityData = async () => {
+    const response = await fetch(config.API_BASE + "getCities");
+    const data = await response.json();
+    if (data.success === false) {
+        getCityData();
+        return;
+    }
+    cities = data.result;
+    let i = 1;
+    cities.forEach((city) => {
+        document.getElementById("c" + i).innerText = city.name;
+        document.getElementById("s" + i).innerText = city.score;
+    });
+};
+
+const getData = async () => {
+    getCityData();
+    await getUserData();
+};
+
+const driver = async () => {
+    await getConfig();
+    await getData();
 };
 
 driver().catch((err) => console.log(err));
