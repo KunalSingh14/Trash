3faae9f333f5d3b199e0320379835e84a3d043bd
Harsh Gupta <harshgupta5901@gmail.com>
2021-09-13 23:28:22 +0530

    added scheme.html link

--- public/script/script2.js
+++ public/script/script2.js
@@ -194,20 +194,20 @@ driver().catch((err) => console.log(err));
 //dropdown
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
-// function myFunction() {
-//     document.getElementById("myDropdown").classList.toggle("show");
-// }
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
 
 // Close the dropdown menu if the user clicks outside of it
-// window.onclick = function (event) {
-//     if (!event.target.matches(".dropbtn")) {
-//         var dropdowns = document.getElementsByClassName("dropdown-content");
-//         var i;
-//         for (i = 0; i < dropdowns.length; i++) {
-//             var openDropdown = dropdowns[i];
-//             if (openDropdown.classList.contains("show")) {
-//                 openDropdown.classList.remove("show");
-//             }
-//         }
-//     }
-// };
+window.onclick = function (event) {
+    if (!event.target.matches(".dropbtn")) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains("show")) {
+                openDropdown.classList.remove("show");
+            }
+        }
+    }
+};
